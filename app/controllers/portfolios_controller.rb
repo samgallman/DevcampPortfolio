4712a535f5d4735a1ed6775d8b106e84cd30dea5
Sam Gallman <samgangallman@gmail.com>
2017-07-27 20:22:31 +0000

    controlling

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,7 @@
 class PortfoliosController < ApplicationController
-  before_action :set_portfolio_item, only: [:edit, :destroy, :show, :update]
+  before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
   layout 'portfolio'
+  access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
   
   def index
     @portfolio_items = Portfolio.all
@@ -28,11 +29,9 @@ class PortfoliosController < ApplicationController
   end
 
   def edit
-
   end
 
   def update
-
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
@@ -46,7 +45,6 @@ class PortfoliosController < ApplicationController
   end
 
   def destroy
-
     # Destroy/delete the record
     @portfolio_item.destroy
 
@@ -65,9 +63,8 @@ class PortfoliosController < ApplicationController
                                       technologies_attributes: [:name]
                                      )
   end
-  
+
   def set_portfolio_item
     @portfolio_item = Portfolio.find(params[:id])
   end
-
 end
