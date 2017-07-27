69eeb27a0927c9346775388cf5477a75cbc242b1
Sam Gallman <samgangallman@gmail.com>
2017-07-27 16:13:07 +0000

    new gem file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,3 @@ class ApplicationController < ActionController::Base
   include CurrentUserConcern
   include DefaultPageContent
 end
-
-  
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+  
+  def copyright_generator
+     GallmanViewTool::Renderer.copyright 'Samantha Gallman', 'All rights reserved'
+  end
 end
