afb9d1aafe322f564763beb7dacdb96bc73033a2
Sam Gallman <samgangallman@gmail.com>
2017-08-02 21:31:12 +0000

    haaha

---
+++ app/assets/channels/application_cable/connection.rb
@@ -0,0 +1,21 @@
+module ApplicationCable
+  class Connection < ActionCable::Connection::Base
+ identified_by :current_user
+ 
+ def guest_user
+   guest = Guest.user.new
+   guest.id = guest.object_id
+ 
+ 
+ def connect
+   self.current_user = find_verified_user || guest_user
+   logger.add_tags 'ActionCable', current_user.email
+   logger.add_tags 'ActionCable', current_user.id
+ end
+ 
+ protected
+ 
+ def find_verified_user
+   if verified_user = env['warden'].user
+     verified_user
+     end
